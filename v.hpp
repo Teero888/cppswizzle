b884e4953db3af706f7f2f99ad712332bd562606
Teero888 <teero484@gmail.com>
2025-11-15 11:54:22 +0100

    V

--- v.hpp
+++ v.hpp
@@ -1,5 +1,5 @@
-#ifndef VEC3_HPP
-#define VEC3_HPP
+#ifndef V_HPP
+#define V_HPP
 
 struct vec2;
 struct vec3;
@@ -175,4 +175,4 @@ SwizzleProxy3<I0, I1, I2>::operator=(const vec3 &other) {
   return *this;
 }
 
-#endif // VEC3_HPP
+#endif // V_HPP
